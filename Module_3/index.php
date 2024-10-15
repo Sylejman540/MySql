60555235bf26c1f84b2c16bcf15241d6a2138d50
SylejmanDurguti <durgutisylejman00@gmail.com>
2024-10-15 19:12:40 +0200

    Create index.php

---
+++ Module_3/index.php
@@ -0,0 +1,16 @@
+<?php
+   $the_string = 'Digital School';
+
+      $the_string = 'Digital School';
+
+      echo strlen(string: "$the_string \n");
+
+
+   $programming = 'Programming is not cool';
+
+   echo str_replace(search: "not", replace:"very", subject: $programming);
+  
+  
+   $another_string = 'Programming';
+   echo strrev(string: $another_string);
+?>
